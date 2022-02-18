5272f178ee77fd1cac780ce3998bf79275b6e94a
GitHub Action <action@github.com>
2022-02-18 09:03:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/ihowcool/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ihowcool/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ihowcool.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ihowcool.ideaplugin.MyBundle
 
 class MyApplicationService {
 
